daf72958578b94fb8fa45149488ad1a51f289b71
BenjaPerezG <baperez@miuandes.cl>
2020-10-25 19:55:07 -0300

    feat: player's light radius reduced when blind

---
+++ Infiltrados/Assets/Scripts/Player Scripts/PlayerLightsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Rendering.LWRP;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerLightsController : MonoBehaviour
+{
+    private float lightRadius;
+    private GameObject playerLight;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D lightParams;
+
+    private void Start()
+    {
+        playerLight = GameObject.Find("Player Light");
+        lightParams = playerLight.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    public void ManageLight(bool isBlind)
+    {
+        if (isBlind)
+        {
+            lightRadius = (float)2.5;
+        }
+        else { lightRadius = 5; }
+        lightParams.pointLightOuterRadius = lightRadius;
+    }
+}
---
+++ Infiltrados/Assets/Scripts/Player Scripts/PlayerPhysicsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysicsController : MonoBehaviour
+{
+
+    public Vector2 movementVector;
+    public float velocity = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Move()
+    {
+        if (movementVector.x != 0 || movementVector.y != 0)
+        {
+            transform.Translate(new Vector3(movementVector.x * Time.deltaTime * velocity, movementVector.y * Time.deltaTime * velocity, 0));
+        }
+    }
+}
